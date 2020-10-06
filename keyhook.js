818abf0d08053d3f4ee45f36fc192432d7119919
Matej Sychra <suculent@me.com>
2020-10-06 14:45:44 +0200

    added input file validation

--- keyhook.js
+++ keyhook.js
@@ -1,10 +1,17 @@
+const fs = require('fs-extra');
+
 let Server = require("./lib/server");
 let config_file = require("./keyhook.json");
 
 for (let j = 0; j < process.argv.length; j++) {
   let name = process.argv[j];
   if ((name == "--config") || (name == "-c")) {
-    config_file = process.argv[j+1];
+    // Check if file exists
+    if (fs.existsSync(process.argv[j+1])) {
+      config_file = process.argv[j+1];
+    } else {
+      console.log("Configuration file", name, "does not exist, using defaults..."),
+    }
   }
 }
 
@@ -13,6 +20,6 @@ let server = new Server(config_file);
 server.setup_server();
 server.start_hooks();
 
-if (process.env.ENVIRONMENT == "test") {
+if (process.env.ENVIRONMENT === "test") {
   process.exit(0);
 }
