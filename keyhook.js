35092d568c5198a4380af89d8df8571525cdb4be
Matej Sychra <suculent@me.com>
2020-10-06 14:30:27 +0200

    parametrized configuration file

--- keyhook.js
+++ keyhook.js
@@ -1,8 +1,15 @@
-const Server = require("./lib/server");
+let Server = require("./lib/server");
+let config_file = require("./keyhook.json");
 
-const config_file = require("./keyhook.json");
+for (let j = 0; j < process.argv.length; j++) {
+  let name = process.argv[j];
+  if ((name == "--config") || (name == "-c")) {
+    config_file = process.argv[j+1];
+    console.log("» Using config file", config_file);
+  }
+}
 
-const server = new Server(config_file);
+let server = new Server(config_file);
 
 server.setup_server();
 server.start_hooks();
